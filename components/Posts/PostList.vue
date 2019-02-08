9a756ca9c8c154eb28853a8be772c216419f4dda
Guilherme André Welter <guilherme.a.welter@gmail.com>
2019-02-08 16:46:16 -0200

    Separando lista de postagens e componente separado

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="featured-posts">
+    <PostPreview
+      :id="1"
+      title="Post 1"
+      previewText="Post 1 néeahhh"
+      :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
+    <PostPreview
+      :id="2"
+      :title="'Post 2'"
+      :previewText="'Post 2 néeahhh'"
+      :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview'
+
+export default {
+  components: { PostPreview }
+}
+</script>
+
+<style scoped>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,17 @@
     <section class="intro">
       <h1>Get the latests thech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        :id="1"
-        title="Post 1"
-        previewText="Post 1 néeahhh"
-        :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
-      <PostPreview
-        :id="2"
-        :title="'Post 2'"
-        :previewText="'Post 2 néeahhh'"
-        :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '~/components/Posts/PostPreview'
+import PostList from '~/components/Posts/PostList'
 
 export default {
-  components: { PostPreview }
+  components: { PostList }
 }
 </script>
-
 
 <style scoped>
 .intro {
@@ -57,14 +45,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
