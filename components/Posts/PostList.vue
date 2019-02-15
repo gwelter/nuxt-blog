3590f56dd7cf0a6dcc3c94550a90ca6b8f7c1a74
Guilherme André Welter <guilherme.a.welter@gmail.com>
2019-02-15 17:07:47 -0200

    Reaproveitando form do post para edição e tornando o link dinâmico entre edição e criação

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,11 +2,13 @@
   <section class="featured-posts">
     <PostPreview
       :id="1"
+      :is-admin="isAdmin"
       title="Post 1"
       previewText="Post 1 néeahhh"
       :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
     <PostPreview
       :id="2"
+      :is-admin="isAdmin"
       :title="'Post 2'"
       :previewText="'Post 2 néeahhh'"
       :thumbnail="'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg'" />
@@ -17,7 +19,13 @@
 import PostPreview from '~/components/Posts/PostPreview'
 
 export default {
-  components: { PostPreview }
+  components: { PostPreview },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  components: { AdminPostForm },
+  data () {
+    return {
+      loadedPost: {
+        author: 'Guilherme',
+        title: 'Teste',
+        thumbnailLink: 'https://i2-prod.manchestereveningnews.co.uk/incoming/article14379834.ece/ALTERNATES/s615/050318_LRR_MEN_WomenTech.jpg',
+        content: 'Test Content for this post'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
